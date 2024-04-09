d15c2ba320f1783b3b09922b56da6e032ef5cc30
ДимаДвоянов <dcoiuuu@mail.ru>
2024-04-09 16:11:54 +0700

    Первый коммит

---
+++ app/src/main/java/com/example/kursovaya/LoginWindow.java
@@ -0,0 +1,14 @@
+package com.example.kursovaya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginWindow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_window);
+    }
+}
